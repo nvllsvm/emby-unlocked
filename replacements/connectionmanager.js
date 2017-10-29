fa0232d06c403804cfa04ceda0d5564fbaff458e
Andrew Rabert <draje@nullsum.net>
2017-10-29 07:57:30 -0400

    3.2.35.0

--- replacements/connectionmanager.js
+++ replacements/connectionmanager.js
@@ -271,7 +271,7 @@ define(['events', 'apiclient', 'appStorage'], function (events, apiClientFactory
             return connectUser;
         };
 
-        self._minServerVersion = '3.2.26';
+        self._minServerVersion = '3.2.27';
 
         self.appVersion = function () {
             return appVersion;
