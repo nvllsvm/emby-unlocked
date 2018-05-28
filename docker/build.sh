8ff3a847061fe70c67fb136b1927d17b33cfd829
Andrew Rabert <ar@nullsum.net>
2018-05-28 14:20:30 -0400

    Clone only the most recent files

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -ex
 
-EMBY_VERSION=3.4.1.0
-
 # Testing repo for Mono
 echo http://dl-cdn.alpinelinux.org/alpine/edge/testing >> /etc/apk/repositories
 
@@ -19,21 +17,15 @@ mkdir /build
 
 # install referenceassemblies-pcl
 cd /build
-git clone https://github.com/directhex/xamarin-referenceassemblies-pcl
+git clone --depth 1 https://github.com/directhex/xamarin-referenceassemblies-pcl
 cd xamarin-referenceassemblies-pcl
 install -dm 755 /usr/lib/mono/xbuild-frameworks/.NETPortable/
 cp -dr v4.5 /usr/lib/mono/xbuild-frameworks/.NETPortable/
 
 # build and install emby
 cd /build
-git clone https://github.com/MediaBrowser/Emby Emby
-
-# Workaround for incorrect tag https://github.com/MediaBrowser/Emby/issues/3299
-#git -C Emby checkout $EMBY_VERSION
-git -C Emby checkout e4f1a16550f7d497968d19683812e594785fbcd9
-
-git clone https://github.com/nvllsvm/emby-unlocked emby-unlocked
-git -C emby-unlocked checkout $EMBY_VERSION
+git clone --depth 1 https://github.com/MediaBrowser/Emby Emby
+git clone --depth 1 https://github.com/nvllsvm/emby-unlocked emby-unlocked
 
 
 #export TERM=xterm
