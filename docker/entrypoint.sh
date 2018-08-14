ea5a7774ee93c45a8a5f0307f968a902c985dfd7
Andrew Rabert <ar@nullsum.net>
2018-08-14 19:34:14 -0400

    Consolidate entrypoint

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-chown $PUID:$PGID /config /media
-su-exec $PUID:$PGID \
-    mono /emby/MediaBrowser.Server.Mono.exe \
-    -programdata /config \
-    -ffmpeg /usr/bin/ffmpeg \
-    -ffprobe /usr/bin/ffprobe
